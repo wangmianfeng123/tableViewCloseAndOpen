545176ec61f6dce9c792f403024b033c2ed3e5ae
KK K <wangmianfeng@wangmianfengdemacbook-pro.local>
2018-04-24 20:55:32 +0800

    Initial Commit

---
+++ tableViewCloseAndOpen/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tableViewCloseAndOpen
+//
+//  Created by 王绵峰 on 2018/4/24.
+//  Copyright © 2018年 王绵峰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tableViewCloseAndOpen/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tableViewCloseAndOpen
+//
+//  Created by 王绵峰 on 2018/4/24.
+//  Copyright © 2018年 王绵峰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
